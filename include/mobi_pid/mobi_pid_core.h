1f5cfad1cc1a7c4636ff4c8c4d620343b4701be0
Akkharaphong EKSIRI <akkharaphong.e@bu.ac.th>
2017-11-04 13:16:19 +0700

    Create mobi_pid_core.h

---
+++ include/mobi_pid/mobi_pid_core.h
@@ -0,0 +1,29 @@
+#ifndef SR_MOBI_PID_CORE_H
+#define SR_MOBI_PID_CORE_H
+
+#include "ros/ros.h"
+#include "ros/time.h"
+
+// Custom message includes. Auto-generated from msg/ directory.
+#include <mobi_msgs/PID.h>
+
+// Dynamic reconfigure includes.
+#include <dynamic_reconfigure/server.h>
+// Auto-generated from cfg/ directory.
+#include <mobi_pid/mobiPIDConfig.h>
+
+class mobiPID
+{
+public:
+  MobiPID();
+  ~MobiPID();
+  void configCallback(mobi_pid::mobiPIDConfig &config, double level);
+  void publishMessage(ros::Publisher *pub_message);
+  void messageCallback(const mobi_msgs::PID::ConstPtr &msg);
+
+  double p_;
+  double d_;
+  double i_;
+
+};
+#endif
